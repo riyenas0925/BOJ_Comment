27a2276d1d476982b3bbebdf65527467e4255398
Dongmin Kang <riyenas0925@gmail.com>
2019-03-22 18:22:34 +0900

    code refactoring

--- Chrome Extension Dev/script.js
+++ Chrome Extension Dev/script.js
@@ -1,34 +1,20 @@
-chrome.tabs.executeScript({
-    code:'document.querySelector("#problem-info").innerHTML'
-}, function(result){
-    var problem_info = result[0];
-    document.querySelector('#problem_info').innerHTML = problem_info;
-});
+var problem_info = "";
+var problem_title = "";
+var problem_description = "";
+var problem_input = "";
+var problem_output = "";
 
-chrome.tabs.executeScript({
-    code:'document.querySelector("#problem_title").innerText'
-}, function(result){
-    var problem_title = result[0];
-    document.querySelector('#problem_title').innerText = problem_title;
-});
+function problemParsing(tag){
+    chrome.tabs.executeScript({
+        code:'document.querySelector("' + tag + '").innerHTML'
+    }, function(result){
+        problem = result[0];
+        document.querySelector(tag).innerHTML = problem;
+    });
+}
 
-chrome.tabs.executeScript({
-    code:'document.querySelector("#problem_input").innerText'
-}, function(result){
-    var problem_input = result[0];
-    document.querySelector('#problem_input').innerText = problem_input;
-});
-
-chrome.tabs.executeScript({
-    code:'document.querySelector("#problem_output").innerText'
-}, function(result){
-    var problem_output = result[0];
-    document.querySelector('#problem_output').innerText = problem_output;
-});
-
-chrome.tabs.executeScript({
-    code:'document.querySelector("#problem_description").innerHTML'
-}, function(result){
-    var problem_description = result[0];
-    document.querySelector('#problem_description').innerHTML = problem_description;
-});
+problemParsing("#problem-info");
+problemParsing("#problem_title");
+problemParsing("#problem_description");
+problemParsing("#problem_input");
+problemParsing("#problem_output");
