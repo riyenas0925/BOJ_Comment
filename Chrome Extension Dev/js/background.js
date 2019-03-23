4346381049eec789292ed18c2f3681f19cca1343
Dongmin Kang <riyenas0925@gmail.com>
2019-03-23 19:01:13 +0900

    폴더 구조 변경

--- Chrome Extension Dev/js/background.js
+++ Chrome Extension Dev/js/background.js
@@ -2,16 +2,16 @@ chrome.tabs.onActivated.addListener(function (info) {
     chrome.tabs.get(info.tabId, function (change) {
         if(change.url == undefined){
             chrome.browserAction.setPopup({tabId: info.tabId, popup: ''});
-            chrome.browserAction.setIcon({path: 'icon-disabled.png', tabId: info.tabId});
+            chrome.browserAction.setIcon({path: '../images/icon-disabled.png', tabId: info.tabId});
             console.log('undefined');
         }
         else if (change.url.match(/https:\/\/www\.acmicpc\.net\/problem\/*/) == null) {
             chrome.browserAction.setPopup({ tabId: info.tabId, popup: '' });
-            chrome.browserAction.setIcon({ path: 'icon-disabled.png', tabId: info.tabId });
+            chrome.browserAction.setIcon({ path: '../images/icon-disabled.png', tabId: info.tabId });
         }
         else {
             chrome.browserAction.setPopup({ tabId: info.tabId, popup: 'popup.html' });
-            chrome.browserAction.setIcon({ path: 'icon.png', tabId: info.tabId });
+            chrome.browserAction.setIcon({ path: '../images/icon.png', tabId: info.tabId });
         }
     });
 });
@@ -22,10 +22,10 @@ chrome.tabs.onUpdated.addListener(function (tabId, change, tab) {
     }
     else if (tab.url.match(/https:\/\/www\.acmicpc\.net\/problem\/*/) == null) {
         chrome.browserAction.setPopup({ tabId: tabId, popup: '' });
-        chrome.browserAction.setIcon({ path: 'icon-disabled.png', tabId: tabId });
+        chrome.browserAction.setIcon({ path: '../images/icon-disabled.png', tabId: tabId });
     }
     else {
         chrome.browserAction.setPopup({ tabId: tabId, popup: 'popup.html' });
-        chrome.browserAction.setIcon({ path: 'icon.png', tabId: tabId });
+        chrome.browserAction.setIcon({ path: '../images/icon.png', tabId: tabId });
     }
 });
