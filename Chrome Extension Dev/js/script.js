1e454c3dc5f1c552b84b7b1fb2a8f9e7e77dc17b
shinkeonkim <singun11@kookmin.ac.kr>
2022-06-27 22:36:57 +0900

    feat: 코드 개선 및 python 주석 추가

--- Chrome Extension Dev/js/script.js
+++ Chrome Extension Dev/js/script.js
@@ -1,30 +1,47 @@
-﻿var problemComments = new Array("","","","","","");
-var i = 0;
+﻿let problemComments = new Array("","","","","","");
+let i = 0;
 
-var codeLanguage = new Array(
-    "",
-    "#include<stdio.h>\n\nint main(void) {\n\n\treturn 0;\n}",
-    "#include<iostream>\n#include<string>\n#include<algorithm>\nusing namespace std;\n\nint main(void) {\n\n\treturn 0;\n}",
-    "public class Main {\n\n\tpublic static void main(String[] args) {\t\n\n\t}\n}"
-)
 
-function problemParsing(tag, selector){
+let codeLanguage = {
+    default: '',
+    c: '#include<stdio.h>\n\nint main(void) {\n\n\treturn 0;\n}',
+    cpp: '#include<iostream>\n#include<string>\n#include<algorithm>\nusing namespace std;\n\nint main(void) {\n\n\treturn 0;\n}',
+    java: 'public class Main {\n\n\tpublic static void main(String[] args) {\t\n\n\t}\n}',
+    python: '\n'
+}
+
+const getTabId = async () => {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab.id;
+}
+
+
+async function problemParsing(tag, selector){
+    const getInnerHtml = (tag) => document.querySelector(tag).innerHTML;
+    const tabId = await getTabId();
+
 
     if(arguments.length == 1){
-        chrome.tabs.executeScript({
-            code:'document.querySelector("' + tag + '").innerHTML'
-        }, function(result){
-            var problem = result[0];
+        chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            func: getInnerHtml,
+            args: [tag],
+        }, (result) => {
+            let problem = result[0].result;
+
             problemComments[i] = removeTag(problem);
             i++;
 
             document.querySelector(tag).innerHTML = problem;
         });
-    } else{
-        chrome.tabs.executeScript({
-            code:'document.querySelector("' + tag + '").innerHTML'
-        }, function(result){
-            var problem = result[0];
+    } else {
+        chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            func: getInnerHtml,
+            args: [tag],
+        }, (result) => {
+            let problem = result[0].result;
 
             if(selector == null){
                 problemComments[i] = problem;
@@ -38,15 +55,15 @@ function problemParsing(tag, selector){
     }
 }
 
-function time(option){
-    var now = new Date();
+const getCurrentTime = (option) => {
+    const now = new Date();
 
-    var year = now.getFullYear();
-    var month = now.getMonth();
-    var date = now.getDate();
-    var hour = now.getHours();
-    var minute = now.getMinutes();
-    var second = now.getSeconds();
+    const year = now.getFullYear();
+    const month = now.getMonth();
+    const date = now.getDate();
+    const hour = now.getHours();
+    const minute = now.getMinutes();
+    const second = now.getSeconds();
 
     if(option == 0){
         return year + "년 " + month + "월 " + date + "일 " + hour + "시 " + minute + "분 " + second + "초";
@@ -56,8 +73,8 @@ function time(option){
     }
 }
 
-function removeTag(text){
-    text = text.replace(/<br\/>/ig, "\n"); 
+const removeTag = (text) => {
+    text = text.replace(/<br\/>/ig, "\n");
     text = text.replace(/<(sup)([^>]*)>/gi,"^")
     text = text.replace(/<(sub)([^>]*)>/gi,"_")
     text = text.replace(/<(\/)?([a-zA-Z]*)(\s[a-zA-Z]*=[^>]*)?(\s)*(\/)?>/ig, "");
@@ -71,8 +88,8 @@ function removeTag(text){
     return text;
 }
 
-function copyToClipboard(val) {
-    var t = document.createElement("textarea");
+const copyToClipboard = (val) => {
+    let t = document.createElement("textarea");
     document.body.appendChild(t);
     t.value = val;
     t.select();
@@ -80,21 +97,76 @@ function copyToClipboard(val) {
     document.body.removeChild(t);
 }
 
-function createHtml(){
-    alert("HTML 복사되었습니다!")
-    var html = document.documentElement.innerHTML;
+const createHtml = () => {
+    alert("HTML 복사되었습니다!");
+    let html = document.documentElement.innerHTML;
     html = html.replace(/(<button[^>]+[>])([^<]*)(<\/button>)/g, "");
     html = html.replace(/(<select[^>]+[>])([^]*)(<\/select>)/g, "");
     copyToClipboard(html);
 }
 
-function createCode(){
+const createCode = () => {
     alert("주석이 복사되었습니다!")
-    copyToClipboard(createComments());
+    copyToClipboard(getInitialCode());
+}
+
+const getSelectedLanguage = () => {
+    return document.getElementById("selectLanguage").value;
+}
+
+const initialCodeOfSelectedLanguage = () => {
+    return codeLanguage[getSelectedLanguage()];
+}
+
+const getDefaultComments = () => {
+    let comments = '';
+    comments += '/*********************************************************************\n';
+    comments += '*Title : ' + problemComments[1] + '\n';
+    comments += '*Number : ' + problemComments[2] + '\n';
+    comments += '*Author : ' + problemComments[3] + '\n';
+    comments += '*Description : ' + problemComments[4] + '\n';
+    comments += '*Input : ' + problemComments[5] + '\n';
+    comments += '*Output : ' + problemComments[6] + '\n';
+    comments += '*Start Time : ' + getCurrentTime(0) + '\n';
+    comments += '*End Time : " ' + getCurrentTime(1) + '\n';
+    comments += '*********************************************************************/\n\n';
+
+    return comments;
+}
+
+const getPythonComments = () => {
+    let comments = '';
+    comments += '"""\n';
+    comments += 'Title : ' + problemComments[1] + '\n';
+    comments += 'Number : ' + problemComments[2] + '\n';
+    comments += 'Author : ' + problemComments[3] + '\n';
+    comments += 'Description : ' + problemComments[4] + '\n';
+    comments += 'Input : ' + problemComments[5] + '\n';
+    comments += 'Output : ' + problemComments[6] + '\n';
+    comments += 'Start Time : ' + getCurrentTime(0) + '\n';
+    comments += 'End Time : ' + getCurrentTime(1) + '\n';
+    comments += '"""\n';
+
+    return comments;
+}
+
+const getInitialCode = () => {
+    const language = getSelectedLanguage();
+
+    let code = ''
+
+    if (language == 'python') {
+        code += getPythonComments();
+    } else {
+        code += getDefaultComments();
+    }
+    code += initialCodeOfSelectedLanguage();
+
+    return code;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    var htmlbtn = document.getElementById('htmlbtn');
+    let htmlbtn = document.getElementById('htmlbtn');
 
     htmlbtn.addEventListener('click', function() {
         createHtml();
@@ -102,34 +174,12 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-    var codebtn = document.getElementById('codebtn');
+    let codebtn = document.getElementById('codebtn');
 
     codebtn.addEventListener('click', function() {
         createCode();
     });
 });
-
-function createComments(){
-    var comments = "";
-    comments += "/*********************************************************************\n";
-    comments += "*Title : " + problemComments[1] + "\n";
-    comments += "*Number : " + problemComments[2] + "\n";
-    comments += "*Author : " + problemComments[3] + "\n";
-    comments += "*Description : " + problemComments[4] + "\n";
-    comments += "*Input : " + problemComments[5] + "\n";
-    comments += "*Output : " + problemComments[6] + "\n";
-    comments += "*Start Time : " + time(0) + "\n";
-    comments += "*End Time : " + time(1) + "\n";
-    comments += "*********************************************************************/\n\n";
-    comments += selectLanguage();
-    
-    return comments;
-}
-
-function selectLanguage(){
-    var i = document.getElementById("selectLanguage").value;
-    return codeLanguage[i];
-}
 
 problemParsing("#problem-info");
 problemParsing("#problem_title");
